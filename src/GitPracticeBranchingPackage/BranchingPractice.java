03a7a3223cb379c95d0146fd7897b87a7bec68ba
hussaini <habib.hussaini@tekschool.us>
2022-11-14 11:20:34 -0800

    commit panthers branch

--- src/GitPracticeBranchingPackage/BranchingPractice.java
+++ src/GitPracticeBranchingPackage/BranchingPractice.java
@@ -4,7 +4,7 @@ public class BranchingPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("this is inside the panthers branch");
 	}
 
 }
